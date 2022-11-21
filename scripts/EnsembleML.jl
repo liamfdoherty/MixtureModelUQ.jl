2a4f9b46f13990f19d87085254cc21c14f09597e
liamfdoherty <lfd27@icloud.com>
2022-11-21 17:36:14 -0500

    Made plots for ensemble class probabilities

--- scripts/EnsembleML.jl
+++ scripts/EnsembleML.jl
@@ -1,18 +1,30 @@
 using Distributions, MLJ
 using MixtureModelUQ
+using Random; Random.seed!(1)
 
-components = [Normal(-0.1, 0.1), Normal(0.1, 0.1)]
+components = [Normal(-0.1, 0.2), Normal(0.1, 0.2)]
 
 weight = [0.7, 0.3]
 mixture = GMM(components, weight)
 
 LC = @load LogisticClassifier pkg = MLJLinearModels
 
-ensemble = MixtureModelUQ.EnsembleModel(LC, 10)
+ensemble = MixtureModelUQ.EnsembleModel(LC, 50)
 
 construct_machines!(ensemble, mixture, 1000)
 
 MixtureModelUQ.fit!(ensemble)
 
 synthetic_data = LinRange(-1, 1, 101)
-predictions = MixtureModelUQ.predict(ensemble, synthetic_data, mode = true)
+mode_choice = false
+predictions = MixtureModelUQ.predict(ensemble, synthetic_data, mode = mode_choice)
+
+if mode_choice == false
+    positive_probabilities = [[prediction.prob_given_ref[1] for prediction in model_predictions] for model_predictions in predictions]
+    
+    display(plot(synthetic_data, positive_probabilities, title = "Probability of Positive Class for IID Models",
+                xlabel = "x", ylabel = "P(k = 1)", legend = :none))
+
+    display(plot(synthetic_data, mean(positive_probabilities), ribbon = std(positive_probabilities),
+                title = "Mean Confidence with One Standard Deviation", xlabel = "x", ylabel = "P(k = 1)", legend = :none))
+end
--- scripts/SampleGMM.jl
+++ scripts/SampleGMM.jl
@@ -1,7 +1,7 @@
 using StatsPlots, Distributions, DataFrames
 using MixtureModelUQ
 
-components = [Normal(-0.1, 0.1), Normal(0.1, 0.1)]
+components = [Normal(-0.1, 0.2), Normal(0.1, 0.2)]
 
 weight1 = [0.7, 0.3]; weight2 = [0.3, 0.7]
 
@@ -9,7 +9,7 @@ model1 = GMM(components, weight1)
 model2 = GMM(components, weight2)
 
 # Collect samples for both models
-N_samples = 1000
+N_samples = 10000
 
 model1_data = DataFrame(samples = Float64[], components = Float64[])
 for i = 1:N_samples
@@ -26,3 +26,9 @@ end
 # Visualize data
 display(histogram(model1_data.samples, title = "Model 1"))
 display(histogram(model2_data.samples, title = "Model 2"))
+
+# Estimate relative entropy
+f(x) = weight1[1]*pdf(components[1], x) + weight1[2]*pdf(components[2], x)
+g(x) = weight2[1]*pdf(components[1], x) + weight2[2]*pdf(components[2], x)
+x = model1_data.samples
+rel_entropy = mean(log.(f.(x) ./ g.(x)))
